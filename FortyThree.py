68e4cdd1e32a63ae7a9e39fd22b28b03b7c9a112
DominikThomas <dominik.v.thomas@gmail.com>
2018-03-28 23:30:00 +0200

    Add the option to smooth the loaded data.
    Use the config file values to create the energy bins corresponding to the loaded data.
    Create a variable with the first derivative of the data, for detection of peaks.
    Detect peaks and save their widths, remove duplicates

--- FortyThree.py
+++ FortyThree.py
@@ -1,10 +1,13 @@
 import numpy, os, glob
+vaha=50
+sirka=2
+vyhlazeni="NE"
 path0='/media/dominik/Windows8_OS/Deimos32python/'
 os.chdir(path0)
 cfgname=glob.glob(path0 + '/*.cfg')[0]
 f0=open(cfgname)
-for x in range(0, 20):
-    if x==12:
+for i0 in range(0, 20):
+    if i0==12:
         config0=(f0.readline().split())
         config1=(f0.readline().split())
     f0.readline()
@@ -13,9 +16,67 @@ newconfig1=(float(config1[1])-float(config0[1]))/(float(config1[0])-float(config
 path1=path0+'spc_frk'
 os.chdir(path1)
 soubor=glob.glob(path1 + '/*.FRK')
-for i0 in range(0, len(soubor)):
+for i1 in range(0, len(soubor)):
     Y0=[0]*8192
-    f1=open(soubor[i0])
-    for i1 in range(0, 8192):
-        Y0[i1]=int(''.join(f1.readline().split()))
+    f1=open(soubor[i1])
+    for i2 in range(0, len(Y0)):
+        Y0[i2]=float(''.join(f1.readline().split()))
     Y=[0]*8192
+    if(vyhlazeni=="ANO"):
+        for i3 in range(1, len(Y0)-1):
+            Y[i3]=(Y0[i3-1]+vaha*Y0[i3]+Y0[i3+1])/(2+vaha)
+        del(i3)
+    elif(vyhlazeni=="NE"):
+        Y=Y0
+    else:
+        print("Chyba parametru 'vyhlazeni'. Nutno zadat ANO nebo NE")
+        break
+    C2=[0]*8192
+    C=[0]*8192
+    Z=[0]*8192
+    C[0]=1
+    C2[0]=newconfig0+newconfig1
+    for i4 in range(1, len(C2)):
+        C[i4]=i4+1
+        C2[i4]=newconfig0+(i4+1)*newconfig1
+        Z[i4]=Y[i4]-Y[i4-1]
+    G0=[]
+    G1=[]
+    H0=[]
+    H1=[]
+    for i5 in range(0, len(Z)-sirka):
+        if(Z[i5+sirka]<0):
+            G0.append(C[i5])
+            G1.append(C2[i5])
+    for i6 in range(0, len(G0)-sirka):
+        l1=G0[i6]
+        l2=G0[i6+sirka]
+        while True:
+            l1=l1-1
+            if(l1==0 or Z[max(l1,1)]<-0.1):
+                break
+        if l1==0:
+            l1=1
+        H0.append(l1)
+        if(l2<len(C2)):
+            while True:
+                l2=l2+1
+                if(l2==len(C2) or Z[l2]>0.1):
+                    break
+            H1.append(l2-1)
+        else:
+            H1.append(l2)
+    H01=[H0[0]]
+    H11=[H1[0]]
+    for i7 in range (1, len(H0)):
+        if (H0[i7-1],H1[i7-1])!=(H0[i7],H1[i7]):
+            H01.append(H0[i7])
+            H11.append(H1[i7])
+        
+        
+        
+    
+#del(i0,i1,i2,Y,Y0,C,C2)
+
+    
+    
